a61f364df8ff2d5f9688d2017cc3eb18b45facb1
ArtyomKingmang <kingmangapp@gmail.com>
2024-01-04 14:53:52 +0300

    display test

--- src/com/kingmang/glengine/Main.java
+++ src/com/kingmang/glengine/Main.java
@@ -1,9 +1,21 @@
 package com.kingmang.glengine;
 
+
 import com.kingmang.glengine.display.Display;
 
-public class Main{
-    public static void main(String args){
+public class Main {
+    public static void main(String[] args){
+        //Создание экрана
+        Display display = new Display(500,500, "title");
+        display.create();
+
+        //Обновление экрана
+        while(!display.shouldClose()){
+            display.update();
+            display.swapBuffers();
+        }
+
 
     }
+
 }
